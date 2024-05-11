c0e9f4247fbc3c3f2cd594c12ee9b74e8c6fc0e3
Yan-Xuan-4832 <123.xuanyan@gmail.com>
2024-05-12 04:53:44 +0800

    ranged attack v1

--- Assets/Scripts/Enemy/EnemyProjectile.cs
+++ Assets/Scripts/Enemy/EnemyProjectile.cs
@@ -4,8 +4,24 @@ public class EnemyProjectile : EnemyDamage
 {
     public float horizontal = 1f;
     public float speed = 8f;
+    private GameObject enemyObj = null;
+    private GameObject projectile = null;
     public Animator animatior;
     [SerializeField] private Rigidbody2D rb;
+    void Start()
+    {
+        projectile= GameObject.Find("projectile");
+        rb = GetComponent<Rigidbody2D>();
+        enemyObj = GameObject.Find("RangedEnemy");
+        horizontal = enemyObj.GetComponent<Ranged_Attack>().horizontal;
+        
+        this.transform.position = new Vector3(enemyObj.transform.position.x + 2* horizontal, 
+        enemyObj.transform.position.y, enemyObj.transform.position.z);
+        Debug.Log(enemyObj.transform.position.z);
+        this.transform.localScale = new Vector3(-1*horizontal, 1,1);
+        Debug.Log("projectile spawned");
+    }
+
     void Update()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
@@ -17,8 +33,7 @@ public class EnemyProjectile : EnemyDamage
         {
             Health health = collider.GetComponent<Health>();
             health.Damage(damage);
-            Destroy(gameObject);
-
         }
+        projectile.SetActive(false);
     }
 }
